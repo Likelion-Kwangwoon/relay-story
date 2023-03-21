6912a4761cae23f84938ca912f9ddfc10b838456
ekdms5566 <ekdms5566@naver.com>
2023-03-21 20:03:30 +0900

    Feat : Themeprovider, globalStyle 설정

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import { theme } from "./theme";
+import reset from "styled-reset";
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  
+  :root {
+    margin: 0 auto;
+    text-align: center;
+  }
+`;
+
 function App() {
   return (
-    <div className="App">
-      <h1>test2</h1>
-    </div>
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+      </ThemeProvider>
+    </>
   );
 }
 
---
+++ src/theme.js
@@ -0,0 +1,15 @@
+export const theme = {
+  color: {
+    white: "#FFFFFF",
+    bg: "#FDF7EB",
+    main: "#FFE682",
+    sub: "#FFCA5F",
+  },
+  text: {
+    black: "#000000",
+    gray1: "#999999",
+    gray2: "#636363",
+    gray3: "#E4E4E4",
+    brown: "#804C22",
+  },
+};
